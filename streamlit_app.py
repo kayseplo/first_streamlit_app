37c45e4a896a1d05ba0887fccfcd9085a6503a79
kayseplo <146263514+kayseplo@users.noreply.github.com>
2024-01-30 09:49:17 -0700

    Update streamlit_app.py
    fix spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,10 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlist.text_input('What fruit would you like information about?')
 if not fruit_choice:
-  streamlist.error("Please select a fruit to get information.")
+  streamlit.error("Please select a fruit to get information.")
 else:
   back_from_function = get_fruityvice_data(fruit_choice)
-  streamlist.dataframe(back_from_function)
+  streamlit.dataframe(back_from_function)
  
 
 
