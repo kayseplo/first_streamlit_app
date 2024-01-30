5d572da0314e5a64add0ccf09855a24e4349dc18
kayseplo <146263514+kayseplo@users.noreply.github.com>
2024-01-30 09:51:11 -0700

    Update streamlit_app.py
    fix indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,9 +50,9 @@ try:
   fruit_choice = streamlist.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
-else:
-  back_from_function = get_fruityvice_data(fruit_choice)
-  streamlit.dataframe(back_from_function)
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
  
 
 
